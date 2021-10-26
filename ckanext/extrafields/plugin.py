451e91f6dfb3314b67346b7265a8b2577c7be5a6
Thomas Schweppe (HTAG) <thomas.schweppe@hypertegrity.de>
2021-10-26 12:27:34 +0200

    Test

--- ckanext/extrafields/plugin.py
+++ ckanext/extrafields/plugin.py
@@ -1,19 +1,26 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-class ExtrafieldsPlugin(plugins.SingletonPlugin):
+class ExtrafieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IDatasetForm)
     plugins.implements(plugins.IConfigurer)
 
     def _modify_package_schema(self, schema):
         schema.update({
-            'sprache': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
-            'datum_erzeugung': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
-            'datum_revision': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
-            'contributorID': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
-            'plannedAvailability': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
-            'politicalGeocodingLevelURI': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
-            'politicalGeocodingURI': [toolkit.get_validator('ignore_missing'), toolkit.get_converter('convert_to_extras')],
+            'contributorID': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'plannedAvailability': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'politicalGeocodingLevelURI': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'politicalGeocodingURI': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
         })
         return schema
 
@@ -30,20 +37,27 @@ class ExtrafieldsPlugin(plugins.SingletonPlugin):
     def show_package_schema(self):
         schema = super(ExtrafieldsPlugin, self).show_package_schema()
         schema.update({
-            'sprache': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'datum_erzeugung': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'datum_revision': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'contributorID': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'plannedAvailability': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'politicalGeocodingLevelURI': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-            'politicalGeocodingURI': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('ignore_missing')],
-        })
+            'contributorID': [toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')]
+            })
+        schema.update({
+            'plannedAvailability': [toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')]
+            })
+        schema.update({
+            'politicalGeocodingLevelURI': [toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')]
+            })
+        schema.update({
+            'politicalGeocodingURI': [toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')]
+            })
         return schema
 
     def is_fallback(self):
         # Return True to register this plugin as the default handler for
         # package types not handled by any other IDatasetForm plugin.
-        return False
+        return True
 
     def package_types(self):
         # This plugin doesn't handle any special package types, it just
